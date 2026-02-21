6a39188d4b520a6374fa9f09b2bcd43a9b7978be
Alex T. <grompygromp@gmail.com>
2026-02-21 17:51:43 +0200

    Refactor camera and level handler scripts safely (#4)

--- Assets/Scripts/level handlers/LevelHandler.cs
+++ Assets/Scripts/level handlers/LevelHandler.cs
@@ -1,51 +1,65 @@
-﻿using System.Collections;
-using System.Collections.Generic;
-using UnityEngine;
-using UnityEngine.SceneManagement;
+﻿using UnityEngine;
 using UnityEngine.EventSystems;
+using UnityEngine.SceneManagement;
 
-public class LevelHandler : MonoBehaviour {
-
+public class LevelHandler : MonoBehaviour
+{
     public void Menu()
     {
-        SceneManager.LoadScene(0);
-        EventSystem.current.SetSelectedGameObject(null); //for butons, but didn't work
+        LoadSceneAndClearSelection(0);
     }
+
     public void StartOne()
     {
-        EventSystem.current.SetSelectedGameObject(null);
-        SceneManager.LoadScene(1);
-
+        LoadSceneAndClearSelection(1);
     }
+
     public void StartTwo()
     {
         SceneManager.LoadScene(2);
     }
+
     public void StartThree()
     {
         SceneManager.LoadScene(3);
     }
+
     public void StartFour()
     {
         SceneManager.LoadScene(4);
     }
+
     public void StartFive()
     {
-        SceneManager.LoadScene(5);
-        EventSystem.current.SetSelectedGameObject(null);
+        LoadSceneAndClearSelection(5);
     }
+
     public void StartSix()
     {
         SceneManager.LoadScene(6);
     }
+
     public void StartNine()
     {
-
-        SceneManager.LoadScene(9);
-        EventSystem.current.SetSelectedGameObject(null);
+        LoadSceneAndClearSelection(9);
     }
 
-    public void disableButtonTest() {
-        EventSystem.current.SetSelectedGameObject(null);
+    public void disableButtonTest()
+    {
+        ClearCurrentSelection();
+    }
+
+    private void LoadSceneAndClearSelection(int sceneIndex)
+    {
+        SceneManager.LoadScene(sceneIndex);
+        ClearCurrentSelection();
+    }
+
+    private void ClearCurrentSelection()
+    {
+        if (EventSystem.current != null)
+        {
+            EventSystem.current.SetSelectedGameObject(null);
+        }
     }
 }
